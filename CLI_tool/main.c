61b33bf1fecef6934c409dd5fc4bfc1aba9fc654
critical-mistake <paula.rancic11@gmail.com>
2024-10-05 18:48:11 +0200

    readinput almost done

--- CLI_tool/main.c
+++ CLI_tool/main.c
@@ -1,12 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 
 // ./CLI_tool < data.txt > result.txt
 
 void readInput() {
+    char input[100]; 
+    char buff[5];
+    int buff_i = 0;
+    scanf("%s", input);
+    for(int i = 0; i < 100; i++)
+    {
+        char c = input[i];
+        printf("c: %c\n", c);
+        if (c == '\0')
+        {
+            break;
+        }
+        else if(c == ',')
+        {
+            buff_i = 0;
+           // memset(&buff[0], 0, sizeof(buff));
+            int number;
+            sscanf(buff, "%d", &number);
+            printf("num: %d\n", number);
+        }
+        else
+        {
+            buff[buff_i++] = c;
+        }
+        
+    }
+    printf("input: %s\n", input);
 }
 
 void bubbleSort(int *data, int n) {
-    // printf("size of array: %d\n", n);
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
             if (data[i] > data[j]) {
@@ -18,12 +45,8 @@ void bubbleSort(int *data, int n) {
     }
 }
 
-int main ()
+void printResult(int *data, int n)
 {
-    printf("Hello CLI tool. I'm sorting your data now :)\n");
-    int data[] = { 3, 6, 1, 8, 8, -3, 0, 7 };
-    int n = sizeof(data) / 4;
-    bubbleSort(data, n);
     printf("Result: [");
     for (int i = 0; i < n; i++) {
         printf(" %d", data[i]);
@@ -32,5 +55,16 @@ int main ()
         }
     }
     printf(" ]\n");
+
+}
+
+int main ()
+{
+    printf("Hello CLI tool. I'm sorting your data now :)\n");
+    int data[] = { 3, 6, 1, 8, 8, -3, 0, 7 };
+    int n = sizeof(data) / 4;
+    bubbleSort(data, n);
+    // printResult(data, n);
+    readInput();
     return 0;
 }
