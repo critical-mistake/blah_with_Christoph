8da25f76f6a68345ee734659fcc0ff5ef5896c22
Christoph Pickl <christophpickl@users.noreply.github.com>
2024-10-15 19:34:40 +0200

    more OOP

--- cplus/Animal.h
+++ cplus/Animal.h
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+// you can pet it, to make it feel comfy
+class Pettable {
+public:
+    // this simulates an INTERFACE, no implementation, just abstract/virtual methods
+    virtual void petMe(int forceInNewton) = 0;
+};
+
 class Animal {
 public:
     int age;
@@ -16,24 +23,51 @@ public:
     ~Animal() {
         cout << "Deconstructing animal" << endl;
     }
-    void scream() {
-        cout << "wow" << endl;
-    }
+    virtual void makeSound() = 0; // making a method virtual => the class will be abstract (=non-instantiable)
 private:
     int secret;
 };
 
-class Dog : public Animal{
+class Dog : public Animal, public Pettable {
 public:
     string name;
     Dog(string name, int age) : Animal(age) { // construct + invoke super-constructor
         // this == Dog*
         this->name = name;
-    }  
+    }
+    void makeSound() {
+        cout << "Wuff!" << endl;
+    }
+    void petMe(int forceInNewton) {
+        //if(force) ..
+        cout << "Wagging the tail while being pet" << endl;
+    }
 };
 
-class Cat : public Animal{
-    public:
-        Cat(int age) : Animal(age){
-        }
+class Cat : public Animal {
+public:
+    Cat(int age) : Animal(age){
+    }
+    void makeSound() {
+        cout << "Meow!" << endl;
+    }
 };
+
+//class Person {
+//};
+//class Student : public Person, public Pettable {
+//};
+//class Professor : public Person {
+//};
+//
+//class A {
+//    void foo();
+//}
+//class B {
+//    void foo();
+//}
+//class C : A, B {};
+//main() {
+//    C c();
+//    c.foo(); // ??? A's or B's?
+//}
--- cplus/main.cpp
+++ cplus/main.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-void showAgeOf(Animal animal);
+void showAgeOf(Animal* animal);
+void beAnnoying(Animal* animal);
 
 int main() {
     string name = "Paula";
@@ -11,17 +12,32 @@ int main() {
 
     Dog bello("Bello", 12);
 
-    bello.scream();
+    bello.makeSound();
 //    cout << "Dog's name and age: " << bello.name << ", " << bello.age << endl;
-    showAgeOf(bello);
+    showAgeOf(&bello);
 
     Cat kitty(5);
-    showAgeOf(kitty);
-    Animal orion(7);
-    showAgeOf(orion);
+//    passByValue(kitty); // kitty will stay 5
+//    passByPointer(kitty); // kitty will become 12
+    showAgeOf(&kitty);
+
+    beAnnoying(&bello);
+    beAnnoying(&kitty);
+    bello.petMe(40);
     return 0;
 }
 
-void showAgeOf(Animal animal) {
-    cout << "Animal's age: " << animal.age << endl;
+//void passByValue(Animal animal) { // copy will be passed; function/stack-local
+//    animal.age = 12;
+//}
+//void passByPointer(Animal* animal) { // access to main's function object directly
+//    animal->age = 12;
+//}
+
+void showAgeOf(Animal* animal) {
+    cout << "Animal's age: " << animal->age << endl;
+}
+
+void beAnnoying(Animal* animal) {
+    animal->makeSound(); // will it be wuff, or meow? determined at runtime!
 }
