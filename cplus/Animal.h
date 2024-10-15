157742dd3c336b013aee1c1603601f84236d1c5f
Christoph Pickl <christophpickl@users.noreply.github.com>
2024-10-15 19:12:13 +0200

    dog

--- cplus/Animal.h
+++ cplus/Animal.h
@@ -1,11 +1,35 @@
 #include <iostream>
 
-class Animal{
+using namespace std;
+
+class Animal {
 public:
     int age;
-    void scream(){
-        std::cout << "wow" <<std::endl;
+    Animal(int age) { // constructor
+        cout << "Constructing animal" << endl;
+        if(age <= 0) {
+            cerr << "Noooooo! Invalid age: " << age << endl;
+            exit(1);
+        }
+        this->age = age;
+    }
+    ~Animal() {
+        cout << "Deconstructing animal" << endl;
+    }
+    void scream() {
+        cout << "wow" << endl;
     }
 private:
     int secret;
 };
+
+class Dog : public Animal {
+public:
+    string name;
+    Dog(string name, int age) : Animal(age) { // construct + invoke super-constructor
+        // this == Dog*
+        this->name = name;
+    }
+};
+
+class Cat
--- cplus/main.cpp
+++ cplus/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream> 
 #include "Animal.h"
+
 using namespace std;
 
+void showAgeOf(Animal animal);
 
-int main(){
+int main() {
     string name = "Paula";
-    cout << "Hello " << name << endl; 
-    Animal bello;
-    bello.age = 12;
+    cout << "Hello " << name << endl;
+
+    Dog bello("Bello", 12);
+
     bello.scream();
-    cout << "Bello.age " << bello.age <<endl;
+//    cout << "Dog's name and age: " << bello.name << ", " << bello.age << endl;
+    showAgeOf(bello);
+
+    Cat ..
+    showAgeOf(kitty)
+
     return 0;
+}
+
+void showAgeOf(Animal animal) {
+    cout << "Animal's age: " << animal.age << endl;
 }
